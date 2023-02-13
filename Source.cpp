ee21fec2736b8867d1d7c15193c4aac1faaeae21
Cyprian Gibas <cyprian.gibas@o2.pl>
2023-02-13 21:51:37 +0100

    Koniec tut4 File IO, działa okej

--- Source.cpp
+++ Source.cpp
@@ -79,53 +79,29 @@ namespace chili
 
 int main()
 {
-	std::ifstream in("derp.txt");
+	// const char data[] = "abc\n\n123";
+	// std::ofstream out("test.txt", std::ios::binary);
+	// // jeœli zapisujemy dane do pliku które zawieraj¹ tylko liczby ipt. to powinno siê je zapisywaæ w formacie std::ios::binary  //
+	// for ( char c: data )
+	// {
+	// 	if (c == 0)
+	// 	{
+	// 		break;
+	// 	}
+	// 	out.put(c);
+	// }
+	// 
+	// std::ofstream out2("boi.dat", std::ios::binary);
+	// const int myInt = 69420;
+	// out2.write(reinterpret_cast<const char*>(&myInt), sizeof(myInt)); // podajemy rozmiar zmiennej( int w c++ to 4 bajty (32 bity))
+	// // do funkcji read i write z biblioteki fstream trzeba stosowaæ reinterpret_cast<char*>, poniewa¿ przyjmuj¹ one wskaŸnik jako parametr
 
-	if (!in)
-	{
-		chili::print( "\n\n Failed to open file" );
-		while (!_kbhit());
-		return -1;
-	}
-
-	in.seekg(0, std::ios::end);
-	const int length = in.tellg();
-	chili::print(" File size is: ");
+	std::ifstream in2("boi.dat", std::ios::binary); // odczytywanie danych, które wpisaliœmy wczeœniej do pliku
+	int data2;
+	in2.read(reinterpret_cast<char*>(&data2), sizeof(data2)); 
 	char buffer[10];
-	chili::int2str(length, buffer, 10);
+	chili::int2str(data2, buffer, 10);
 	chili::print(buffer);
-	chili::print("\n\n");
-
-	in.seekg(0, std::ios::beg);
-
-	for (char c = in.get() ; in.good() ; c = in.get())
-	{
-		_putch( c );
-	}
-
-	if( in.bad())
-	{
-		chili::print( "\n\n Bad shit happend" );
-	}
-	else if(in.eof())
-	{
-		chili::print( "\n\n Successfully reached end of file" );
-	}
-	else
-	{
-		chili::print( "\n\n Some kind of fail?" );
-	}
-
-	std::ofstream out("out.txt");
-	chili::print("\n\n");
-
-	for (char c = _getch(); c != 13 ; c = _getch())
-	{
-		_putch(c);
-		out.put(c);
-	}
-	out.seekp(10);
-
 
 	while (!_kbhit());
 	return 0;
